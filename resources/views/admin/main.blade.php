623e34bbbd8507020a9f886ea4dc565b849ae535
a man well <aemmanuel199@gmail.com>
2018-05-13 20:01:59 +0300

    deleting unwanted images

--- resources/views/admin/main.blade.php
+++ resources/views/admin/main.blade.php
@@ -89,7 +89,7 @@
             <li class="dropdown user user-menu">
               <a href="#" class="dropdown-toggle" data-toggle="dropdown">
               <img src="/imgs/emma.jpg" class="user-image" alt="User Image">
-              <span class="hidden-xs">emmanuel</span>
+              <span class="hidden-xs">{{Auth::user()->name}}</span>
             </a>
               <ul class="dropdown-menu">
                 <!-- Menu Body -->
@@ -120,7 +120,7 @@
             <img src="/imgs/emma.jpg" class="img-circle" alt="User Image">
           </div>
           <div class="pull-left info">
-            <p>emmanuel</p>
+            <p>{{Auth::user()->name}}</p>
             <a href="#"><i class="fa fa-circle text-success"></i> Online</a>
           </div>
         </div>
