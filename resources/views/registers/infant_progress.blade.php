2c6a6f190e7c02ad8fac02947ec0b3f20eaaf7e0
emma255 <aemmanuel199@gmail.com>
2018-05-31 04:52:34 +0300

    edit form

--- resources/views/registers/infant_progress.blade.php
+++ resources/views/registers/infant_progress.blade.php
@@ -37,109 +37,69 @@
                                 <br>
 
                                 <div class="row">
-                                    <div class="col-md-5">
+                                    <div class="col-md-4">
                                         <label>Mahudhurio</label>
                                         <select name="hudhurio" class="form-control">
                                             <option value="" hidden=""></option>
-											<option value="Masaa 48">Ndani ya Masaa 48</option>
-											<option value="Siku 3-7">Ndani ya Siku 3-7</option>
-											<option value="Siku 8-28">Ndani ya Siku 8-28</option>
-											<option value="Siku 29-42">Ndani ya Siku 29-42</option>
+											<option value="Masaa 48">Masaa 48</option>
+											<option value="Siku 3-7">Siku 3-7</option>
+											<option value="Siku 8-28">Siku 8-28</option>
+											<option value="Siku 29-42">Siku 29-42</option>
                                         </select>
                                     </div>
 
                                     <div class="col-md-4">
-                                        <label>Tarehe</label>
+                                        <label>Tarehe ya hudhurio</label>
                                         <input type="date" class="form-control" name="tarehe">
                                     </div>
-
-                                    <div class="col-md-3">
-                                        <label>Joto (&deg;C)</label>
-                                        <input type="number" class="form-control" name="joto" min="0">
-                                    </div>
-                                </div>
-
-                                <br>
-
-                                <div class="row">
-                                    <div class="col-md-3">
-                                        <label for="uzito">Uzito (Gram)</label>
-                                        <input type="number" name="uzito" id="uzito" class="form-control" min="0">
-                                    </div>
-                                    <div class="col-md-5">
-                                        <label>Utando mweupe mdomoni</label>
-                                        <select name="utando_mweupe_mdomoni" class="form-control">
-                                            <option value="" hidden=""></option>
-                                            <option value="Ndio">Ndio</option>
-                                            <option value="Hapana">Hapana</option>
-                                        </select>
-                                    </div>
                                     <div class="col-md-4">
-                                        <label>Kuchezacheza mtoto</label>
-                                        <select name="kuchezacheza_mtoto" class="form-control">
-                                            <option value="" hidden=""></option>
-                                            <option value="Kawaida">Kawaida</option>
-                                            <option value="Kidogo kuliko Kawaida">Kidogo</option>
-                                        </select>
+                                        <label for="tarehe_ya_kurudi">Tarehe ya kurudi</label>
+                                        <input type="date" name="tarehe_ya_kurudi" id="tarehe_ya_kurudi" class="form-control">
                                     </div>
                                 </div>
 
                                 <br>
 
-
-                                <div class="row">
-
-                                </div>
-
-                                <br>
-
-                                <div class="row">
-                                    <div class="col-md-4">
-                                        <label>Uchafu machoni</label>
-                                        <select name="macho_yanatoa_uchafu" class="form-control">
-                                            <option value="" hidden=""></option>
-                                            <option value="Ndio">Ndio</option>
-                                            <option value="Hapana">Hapana</option>
-                                        </select>
-                                    </div>
-
-                                    <div class="col-md-4">
-                                        <label>Wekundu wa damu</label>
-                                        <select name="wekundu_wa_damu" class="form-control">
-                                            <option value="" hidden=""></option>
-                                            <option value="Ndio">Umepungua</option>
-                                            <option value="Hapana">Kawaida</option>
-                                        </select>
-                                    </div>
-
-                                    <div class="col-md-4">
-                                        <label>Vipele vyenye usaha</label>
-                                        <select name="ngozi_ina_vipele_vyenye_usaha" class="form-control">
-                                            <option value="" hidden=""></option>
-                                            <option value="Ndio">Ndio</option>
-                                            <option value="Hapana">Hapana</option>
-                                        </select>
-                                    </div>
-                                </div>
-
-                                <br>
-
-                                <div class="row">
-                                    <div class="col-md-6">
-                                        <label>Ngozi imebadilika kuwa njano</label>
-                                        <select name="ngozi_njano" class="form-control">
-                                            <option value="" hidden=""></option>
-                                            <option value="Ndio">Ndio</option>
-                                            <option value="Hapana">Hapana</option>
-                                        </select>
-                                    </div>
-                                    <div class="col-md-6">
-                                        <label for="matatizo_mengine">Matatizo mengine</label>
-                                        <textarea name="matatizo_mengine" id="matatizo_mengine" rows="1" class="form-control"></textarea>
+                                <div class="card">
+                                    <div class="card-header"><label for="">VIPIMO NA HUDUMA MUHIMU</label></div>
+                                    <div class="card-body">
+                                        <div class="row">
+                                            <div class="col-md-3">
+                                                <label for="uzito">Uzito (Gram)</label>
+                                                <input type="number" name="uzito" id="uzito" class="form-control" min="0">
+                                            </div>
+                                            <div class="col-md-3">
+                                                <label>Joto (&deg;C)</label>
+                                                <input type="number" class="form-control" name="joto" min="0">
+                                            </div>
+                                            <div class="col-md-6">
+                                                <label for="kmc">Huduma ya kangaroo (KMC)</label>
+                                                <select name="kmc" class="form-control">
+                                                    <option value="" hidden=""></option>
+                                                    <option value="Ndio">Ndio</option>
+                                                    <option value="Hapana">Hapana</option>
+                                                </select>
+                                            </div>
+                                        </div>
+                                        <br>
+                                        <div class="row">
+                                            <div class="col-md-3">
+                                                <label for="hb">HB (g/dl)</label>
+                                                <input type="number" name="hb" id="hb" class="form-control">
+                                            </div>
+                                            <div class="col-md-9">
+                                                <label for="lishe">Lishe ya mtoto</label>
+                                                <select name="lishe" class="form-control">
+                                                    <option value="" hidden=""></option>
+                                                    <option value="EBF">Maziwa ya mama pekee (EBF)</option>
+                                                    <option value="RF">Maziwa mbadala (RF)</option>
+                                                    <option value="MF">Maziwa ya mama na chakula kingine (MF)</option>
+                                                </select>
+                                            </div>
+                                        </div>
+                                        <br>
                                     </div>
                                 </div>
-
-                                <br>
 
                             </div>
                             <!--end left division-->
@@ -147,78 +107,65 @@
 
                             <!--right division-->
                             <div class="col-md-5">
-                                <div class="col-md-12 row">
-                                    <div class="col-md-3">
-                                        <label for="hb">HB (g/dl)</label>
-                                        <input type="number" name="hb" id="hb" class="form-control">
-                                    </div>
-                                    <div class="col-md-4">
-                                        <label for="lishe">Lishe</label>
-                                        <select name="lishe" class="form-control">
-                                            <option value="" hidden=""></option>
-                                            <option value="EBF">EBF</option>
-                                            <option value="RF">RF</option>
-                                            <option value="MF">MF</option>
-                                        </select>
-                                    </div>
-                                    <div class="col-md-5">
-                                        <label for="tarehe_ya_kurudi">Tarehe ya kurudi</label>
-                                        <input type="date" name="tarehe_ya_kurudi" id="tarehe_ya_kurudi" class="form-control">
-                                    </div>
-                                </div>
-                                <br>
-                                <div class="card col-md-12">
-                                    <div class="card-header">KITOVU</div>
+                                <div class="card">
+                                    <div class="card-header">MAAMBUKIZI KWA MTOTO</div>
                                     <div class="card-body">
                                         <div class="row">
-                                            <div class="col-md-6"><label>Kimepona</label>
-                                                <select name="kitovu_kimepona" class="form-control">
+                                            <div class="col-md-6">
+                                                <label>Uambukizo ngozini</label>
+                                                <select name="maambukizi_ngozini" class="form-control">
                                                     <option value="" hidden=""></option>
                                                     <option value="Ndio">Ndio</option>
                                                     <option value="Hapana">Hapana</option>
                                                 </select>
                                             </div>
-
-                                            <div class="col-md-6"><label>Chekundu</label>
-                                                <select name="kitovu_chekundu" class="form-control">
+                                            
+                                            <div class="col-md-6">
+                                                <label for="uambukizo_mkali">Uambukizo mkali</label>
+                                                <select name="uambukizo_mkali" class="form-control">
                                                     <option value="" hidden=""></option>
                                                     <option value="Ndio">Ndio</option>
                                                     <option value="Hapana">Hapana</option>
                                                 </select>
-                                            </div>
-                                            <div class="col-md-5">
-
                                             </div>
                                         </div>
-
                                         <br>
-
                                         <div class="row">
-                                            <div class="col-md-6"><label>Kinatoa harufu</label>
-                                                <select name="kitovu_kinatoa_harufu" class="form-control">
+                                            <div class="col-md-6">
+                                                <label>Uambukizi machoni</label>
+                                                <select name="maambukizi_machoni" class="form-control">
                                                     <option value="" hidden=""></option>
                                                     <option value="Ndio">Ndio</option>
                                                     <option value="Hapana">Hapana</option>
                                                 </select>
                                             </div>
-
-                                            <div class="col-md-6"><label>Kinatoa usaha</label>
-                                                <select name="kitovu_kinatoa_usaha" class="form-control">
+                                            <div class="col-md-6">
+                                                <label>Uambukizi mdomoni</label>
+                                                <select name="maambukizi_mdomoni" class="form-control">
                                                     <option value="" hidden=""></option>
                                                     <option value="Ndio">Ndio</option>
                                                     <option value="Hapana">Hapana</option>
                                                 </select>
-                                            </div>
-                                            <div class="col-md-5">
-
                                             </div>
                                         </div>
-
+                                        <br>
+                                        <div class="row">
+                                            <div class="col-md-6">
+                                                <label for="maambukizi_kitovu">Maambukizi kitovuni</label>
+                                                <select name="maambukizi_kitovu" class="form-control">
+                                                    <option value="" hidden=""></option>
+                                                    <option value="Ndio">Ndio</option>
+                                                    <option value="Hapana">Hapana</option>
+                                                </select>
+                                            </div>
+                                            <div class="col-md-6">
+                                                <label for="matatizo_mengine">Matatizo mengine</label>
+                                                <input type="text" name="matatizo_mengine" id="matatizo_mengine" class="form-control">
+                                            </div>
+                                        </div>
                                     </div>
                                 </div>
-
                                 <br>
-
                                 <div class="card col-md-12">
                                     <div class="card-header">CHANJO</div>
                                     <div class="card-body">
@@ -242,18 +189,15 @@
 
                         </div>
                     </div>
-                    <!--end right division-->
-
+                        <!--end right division-->
+                <div class="row">
+                    <div class="col-md-5"></div>
+                    <div class="col-md-2"><input type="Submit" name="submit" class="btn btn-primary form-control" value="Hifadhi"></div>
+                    <div class="col-md-5"></div>
+                </div>
             </div>
 
-            <div class="row">
-                <div class="col-md-5"></div>
-                <div class="col-md-2"><input type="Submit" name="submit" class="btn btn-primary form-control" value="Hifadhi"></div>
-                <div class="col-md-5"></div>
-            </div>
             </form>
-            <br>
-
         </div>
 
 
@@ -261,5 +205,4 @@
 
 </div>
 
-</div
 @endsection
