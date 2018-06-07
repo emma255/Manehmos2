46c73078ab8528d9cdcdfb7e093c0751982b4b1d
“[emma255]” <[aemmanuel199@gmail.com>
2018-06-07 21:57:53 +0300

    format

--- resources/views/registers/register7.blade.php
+++ resources/views/registers/register7.blade.php
@@ -1,5 +1,4 @@
-@extends('layouts.main') 
-@section('content')
+@extends('layouts.main') @section('content')
 
 <div class="container">
 	<div class="row">
@@ -20,26 +19,31 @@
 							<div class="col-md-2"></div>
 							<div class="col-md-4">
 								<div class="row col-md-12 form-group">
-									<div class="col-md-6"><label>Namba ya mtoto</label></div>
-									<div class="col-md-6"><input type="text" class="form-control" name="namba_ya_usajili"></div>
+									<div class="col-md-6">
+										<label>Namba ya mtoto</label>
+									</div>
+									<div class="col-md-6">
+										<input type="text" class="form-control" name="namba_ya_usajili">
+									</div>
 								</div>
 								<div class="row card col-md-12 form-group">
 									<div class="card-header">Vitamin A</div>
 									<div class="card-body">
 										<div class="row">
-											<div class="col-md-6"><label>Umri wa mtoto</label>
+											<div class="col-md-6">
+												<label>Umri wa mtoto</label>
 												<select name="vitamin_A_umri" class="form-control">
-							                    <option value="" hidden=""></option>
-							                    <option value="Miezi 9">Miezi 9</option>
-							                    <option value="Miezi 15">Miezi 15</option>
-							                    <option value="Miezi 21">Miezi 21</option>
-							                    <option value="Miezi 27">Miezi 27</option>
-							                    <option value="Miezi 33">Miezi 33</option>
-							                    <option value="Miezi 39">Miezi 39</option>
-							                    <option value="Miezi 45">Miezi 45</option>
-							                    <option value="Miezi 51">Miezi 51</option>
-							                    <option value="Miezi 57">Miezi 57</option>
-							                </select>
+													<option value="" hidden=""></option>
+													<option value="Miezi 9">Miezi 9</option>
+													<option value="Miezi 15">Miezi 15</option>
+													<option value="Miezi 21">Miezi 21</option>
+													<option value="Miezi 27">Miezi 27</option>
+													<option value="Miezi 33">Miezi 33</option>
+													<option value="Miezi 39">Miezi 39</option>
+													<option value="Miezi 45">Miezi 45</option>
+													<option value="Miezi 51">Miezi 51</option>
+													<option value="Miezi 57">Miezi 57</option>
+												</select>
 
 											</div>
 											<div class="col-md-6">
@@ -55,7 +59,9 @@
 								</div>
 
 								<div class="row card col-md-12 form-group">
-									<div class="card-header">Mebendazole / Albendazole <small>Kila miezi 6</small></div>
+									<div class="card-header">Mebendazole / Albendazole
+										<small>Kila miezi 6</small>
+									</div>
 									<div class="card-body">
 										<div class="row">
 											<div class="col-md-6">
@@ -84,7 +90,8 @@
 									</div>
 								</div>
 								<div class="row">
-									<div class="col-md-6"><label>Chanjo ya Surua</label>
+									<div class="col-md-6">
+										<label>Chanjo ya Surua</label>
 										<select name="Surua" class="form-control">
 											<option value="" hidden=""></option>
 											<option value="Surua 1">Surua 1</option>
@@ -103,7 +110,8 @@
 								<div class="col-md-12">
 									<div class="card form-group">
 										<div class="row col-md-12 card-body">
-											<div class="col-md-6"><label>Chanjo ya PENTA</label>
+											<div class="col-md-6">
+												<label>Chanjo ya PENTA</label>
 												<select name="PENTA" class="form-control">
 													<option value="" hidden=""></option>
 													<option value="PENTA 1">PENTA 1</option>
@@ -120,13 +128,14 @@
 									</div>
 									<div class="card form-group">
 										<div class="row col-md-12 card-body">
-											<div class="col-md-6"><label>Chanjo ya Polio</label>
+											<div class="col-md-6">
+												<label>Chanjo ya Polio</label>
 												<select name="Polio" class="form-control">
-												<option value="" hidden=""></option>
-												<option value="Polio 1">Polio 1</option>
-												<option value="Polio 2">Polio 2</option>
-												<option value="Polio 3">Polio 3</option>
-											</select>
+													<option value="" hidden=""></option>
+													<option value="Polio 1">Polio 1</option>
+													<option value="Polio 2">Polio 2</option>
+													<option value="Polio 3">Polio 3</option>
+												</select>
 											</div>
 
 											<div class="col-md-6">
@@ -137,7 +146,8 @@
 									</div>
 									<div class="card form-group">
 										<div class="row col-md-12 card-body">
-											<div class="col-md-6"><label>Chanjo ya PCV13</label>
+											<div class="col-md-6">
+												<label>Chanjo ya PCV13</label>
 												<select name="PCV13" class="form-control">
 													<option value="" hidden=""></option>
 													<option value="PCV13 1">PCV13 1</option>
@@ -154,12 +164,13 @@
 									</div>
 									<div class="card  form-group">
 										<div class="row col-md-12 card-body">
-											<div class="col-md-6"><label>Chanjo ya Rota</label>
+											<div class="col-md-6">
+												<label>Chanjo ya Rota</label>
 												<select name="Rota" class="form-control">
-												<option value="" hidden=""></option>
-												<option value="Rota 1">Rota 1</option>
-												<option value="Rota 2">Rota 2</option>
-											</select>
+													<option value="" hidden=""></option>
+													<option value="Rota 1">Rota 1</option>
+													<option value="Rota 2">Rota 2</option>
+												</select>
 											</div>
 
 											<div class="col-md-6">
@@ -168,7 +179,9 @@
 											</div>
 										</div>
 									</div>
-									<div class="col-md-5"><input type="submit" class="form-control btn-primary" name="save6" value="Hifadhi"></div>
+									<div class="col-md-5">
+										<input type="submit" class="form-control btn-primary" name="save6" value="Hifadhi">
+									</div>
 								</div>
 							</div>
 						</div>
@@ -181,8 +194,7 @@
 		</div>
 	</div>
 </div>
-@endsection
- {{-- rufaa
+@endsection {{-- rufaa
 <div class="col-md-3">
 	<br>
 	<div class="card row">
