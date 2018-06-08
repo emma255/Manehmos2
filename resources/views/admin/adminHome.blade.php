736c7faec6b248e5d083cc293a693eb9e9c639cb
“[emma255]” <[aemmanuel199@gmail.com>
2018-06-08 23:19:48 +0300

    change appearance of admin home page

--- resources/views/admin/adminHome.blade.php
+++ resources/views/admin/adminHome.blade.php
@@ -2,44 +2,6 @@
 @section('content')
 <!-- Main content -->
 
-<section class="col-lg-7 connectedSortable">
-  <!-- Custom tabs (Charts with tabs)-->
-
-
-  <!-- quick email widget -->
-  <div class="box box-info">
-    <div class="box-header">
-      <i class="fa fa-envelope"></i>
-
-      <h3 class="box-title">Quick Email</h3>
-      <!-- tools box -->
-      <div class="pull-right box-tools">
-        <button type="button" class="btn btn-info btn-sm" data-widget="remove" data-toggle="tooltip" title="Remove">
-                  <i class="fa fa-times"></i></button>
-      </div>
-      <!-- /. tools -->
-    </div>
-    <div class="box-body">
-      <form action="#" method="post">
-        <div class="form-group">
-          <input type="email" class="form-control" name="emailto" placeholder="Email to:">
-        </div>
-        <div class="form-group">
-          <input type="text" class="form-control" name="subject" placeholder="Subject">
-        </div>
-        <div>
-          <textarea class="textarea" placeholder="Message" style="width: 100%; height: 125px; font-size: 14px; line-height: 18px; border: 1px solid #dddddd; padding: 10px;"></textarea>
-        </div>
-      </form>
-    </div>
-    <div class="box-footer clearfix">
-      <button type="button" class="pull-right btn btn-default" id="sendEmail">Send
-                <i class="fa fa-arrow-circle-right"></i></button>
-    </div>
-  </div>
-
-</section>
-<!-- /.Left col -->
 <!-- right col (We are only adding the ID to make the widgets sortable)-->
 <section class="col-lg-5 connectedSortable">
 
