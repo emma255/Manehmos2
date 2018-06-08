f916faa129e1b3b04f01867bc60f6d03af5362fd
“[emma255]” <[aemmanuel199@gmail.com>
2018-06-08 18:47:23 +0300

    add district column

--- 2014_10_12_000000_create_users_table.php
+++ 2014_10_12_000000_create_users_table.php
@@ -15,12 +15,13 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
-            $table->string('worker_id');
-            $table->string('position');
-            $table->string('phone_no');
-            $table->string('email');
-            $table->string('center');
+            $table->string('name', 25);
+            $table->string('worker_id', 10);
+            $table->string('position', 20);
+            $table->string('phone_no', 10);
+            $table->string('email',20);
+            $table->string('center',15);
+            $table->string('district',15);
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
