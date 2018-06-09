226831199429f8c168b1b681080447d61de4b3e4
“[emma255]” <[aemmanuel199@gmail.com>
2018-06-09 05:19:14 +0300

    modify route

--- resources/views/auth/registrationForm.blade.php
+++ resources/views/auth/registrationForm.blade.php
@@ -8,7 +8,7 @@
                 <div class="card-body">
                     <p>If you already have an account, click
                         <a href="{{ route('login') }}">{{ __('here') }}</a> to login</p>
-                    <form method="POST" action="{{ route('register') }}">
+                    <form method="POST" action="send/details">
                         @csrf
 
                         <div class="form-group row">
