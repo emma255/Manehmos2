6566a8d52af1459ce410dd5bd44da95359d84c90
“[emma255]” <[aemmanuel199@gmail.com>
2018-06-09 05:20:50 +0300

    create a chnage password form

---
+++ resources/views/admin/changePassword.blade.php
@@ -0,0 +1,52 @@
+@extends('admin.main') @section('content')
+
+
+<!-- Main content -->
+<section class="content">
+    <div class="col-md-6">
+        <!-- Horizontal Form -->
+        <div class="box box-info">
+            <div class="box-header with-border">
+                <h3 class="box-title">Change password for {{$user->name}}</h3>
+            </div>
+            <!-- /.box-header -->
+            <!-- form start -->
+            <form class="row" method="POST" action="password/change">
+                @csrf
+
+                <div class="form-group row">
+                    <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
+
+                    <div class="col-md-6">
+                        <input id="password" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password"
+                            required> @if ($errors->has('password'))
+                        <span class="invalid-feedback">
+                            <strong>{{ $errors->first('password') }}</strong>
+                        </span>
+                        @endif
+                    </div>
+                </div>
+
+                <div class="form-group row">
+                    <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Confirm Password') }}</label>
+                    <div class="col-md-6">
+                        <input id="password-confirm" type="password" class="form-control{{ $errors->has('password_confirmation') ? ' is-invalid' : '' }}"
+                            name="password_confirmation" required> @if ($errors->has('password_confirmation'))
+                        <span class="invalid-feedback">
+                            <strong>{{ $errors->first('password_confirmation') }}</strong>
+                        </span>
+                        @endif
+                    </div>
+                </div>
+
+                <div class="form-group row mb-0">
+                    <div class="col-md-6 offset-md-4">
+                        <button type="submit" class="btn btn-primary">
+                            {{ __('Change Password') }}
+                        </button>
+                    </div>
+                </div>
+            </form>
+        </div>
+</section>
+@endsection
