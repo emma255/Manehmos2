bd93bfa0e8712cc5261e1fc56a2bcde9baadb6c7
“[emma255]” <[aemmanuel199@gmail.com>
2018-06-10 04:03:39 +0300

    adjusting the block

--- resources/views/admin/changePassword.blade.php
+++ resources/views/admin/changePassword.blade.php
@@ -17,7 +17,7 @@
                     <input type="hidden" name="_method" value="PATCH"> @csrf
 
                     <div class="form-group row">
-                        <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
+                        <label for="password" class="col-md-4 col-xs-offset-1 col-form-label text-md-right">{{ __('Password') }}</label>
 
                         <div class="col-md-6">
                             <input id="password" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password"
@@ -29,7 +29,7 @@
                     </div>
 
                     <div class="form-group row">
-                        <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Confirm Password') }}</label>
+                        <label for="password-confirm" class="col-md-4 col-xs-offset-1 col-form-label text-md-right">{{ __('Confirm Password') }}</label>
                         <div class="col-md-6">
                             <input id="password-confirm" type="password" class="form-control{{ $errors->has('password_confirmation') ? ' is-invalid' : '' }}"
                                 name="password_confirmation" required> @if ($errors->has('password_confirmation'))
@@ -40,7 +40,7 @@
                     </div>
 
                     <div class="form-group row mb-0">
-                        <div class="col-md-6 offset-md-4">
+                        <div class="col-md-6 col-xs-offset-7">
                             <button type="submit" class="btn btn-primary">
                                 {{ __('Change Password') }}
                             </button>
