dc247595843bbaac5bc45b0f6d57ec60d86c28fa
“[emma255]” <[aemmanuel199@gmail.com>
2018-06-13 18:09:22 +0300

    arrange methods and make autofile naming

--- app/Http/Controllers/ReportsController.php
+++ app/Http/Controllers/ReportsController.php
@@ -13,6 +13,13 @@ class ReportsController extends Controller
         $this->middleware('clinician');
     }
 
+    // home
+    public function index()
+    {
+        return view('reports.reportHome');
+    }
+
+    //check the name of the report
     public function testcase()
     {
         $vals = request();
@@ -21,28 +28,31 @@ class ReportsController extends Controller
     }
 
 
-
-    public function postanatal()
+// print the postnatal report
+    public function postnatal()
     {
         // $maelezo = Maelezo_view::whereyear('tarehe_ya_hudhurio', $year)->wheremonth('tarehe_ya_hudhurio', $month)->get();
+        $file_name = 'postnatal_report_'.date(" F ", mktime(0, 0, 0, request('month'), 10)).'_'.request('year').'.pdf';
+        session()->flash('flash_message', 'Ripoti imeshaandaliwa!');
 
-        return view('reports.postnatal');
+        $pdf = PDF::loadview('reports.postnatal');
+        return $pdf->download($file_name);
 
     }
 
-
+//print the ivd activities report
     public function ivd_activities()
     {
         echo 'ivd activities';
     }
 
-
+//print ufuatiliaji mtoto report
     public function ufuatiliaji_mtoto()
     {
         echo 'ufuatiliaji mtoto';
     }
 
-
+//print antenatal report
     public function antenatal()
     {
         echo 'antenatal';
@@ -63,12 +73,6 @@ class ReportsController extends Controller
 
 
 
-    // home
-    public function index()
-    {
-        return view('reports.reportHome');
-    }
-
     // ripoti ufuatliaji wa mtoto
     public function mtotoPrint()
     {
@@ -82,8 +86,8 @@ class ReportsController extends Controller
     {
         $values = request();
 
-        // $pdf = PDF::loadview('templates.postnatal', compact('values'));
-        // return $pdf->download('postnatalShow.pdf');
+        $pdf = PDF::loadview('templates.postnatal', compact('values'));
+        return $pdf->download('postnatalShow.pdf');
         
     }
 
@@ -101,4 +105,4 @@ class ReportsController extends Controller
         $pdf = PDF::loadview('reports.antenatal');
         return $pdf->download('antenatal.pdf');
     }
-}
+}        // $maelezo = Maelezo_view::whereyear('tarehe_ya_hudhurio', $year)->wheremonth('tarehe_ya_hudhurio', $month)->get();
