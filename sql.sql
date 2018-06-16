7a9c572c766c383f5c80a3eedfef6b2342bd43c0
“[emma255]” <[aemmanuel199@gmail.com>
2018-06-16 23:05:55 +0300

    something new

--- sql.sql
+++ sql.sql
@@ -104,5 +104,5 @@ INNER JOIN register6s WHERE register_maternals.namba_ya_usajili = register6s.nam
 
 CREATE VIEW antenatal_malaria AS
 SELECT register6s.tarehe_ya_hudhurio, register_maternals.namba_ya_usajili, DATEDIFF(register_maternals.lnmp,register_maternals.tarehe_ya_kuzaliwa) as umri, 
-register6s.llin, register6s.mrdt_o_bs, register6s.ipt FROM register_maternals 
+register6s.llin, register6s.mrdt_o_bs, register6s.ipt, register6s.albendazole_mebendazole, register6s.vidonge_vya_I_FA, register6s.rufaa_alikopelekwa, register6s.sababu_ya_rufaa FROM register_maternals 
 INNER JOIN register6s WHERE register_maternals.namba_ya_usajili = register6s.namba_ya_usajili;
