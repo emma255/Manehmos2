53bf67fd06676bce82175ae0c6cb88704916c4a7
“[emma255]” <[aemmanuel199@gmail.com>
2018-06-22 21:03:31 +0300

    removing unwanted methods

--- app/Http/Controllers/ReportsController.php
+++ app/Http/Controllers/ReportsController.php
@@ -23,29 +23,23 @@ class ReportsController extends Controller
     public function testcase()
     {
         $vals = request();
-
         return redirect()->route(request()->reportType, ['month'=> request()->month, 'year'=>request()->year, 'uses'=>'ReportsController@postanatal']);
     }
-
 
 // print the postnatal report
     public function postnatal()
     {
-        // $maelezo = Maelezo_view::whereyear('tarehe_ya_hudhurio', $year)->wheremonth('tarehe_ya_hudhurio', $month)->get();
         $file_name = 'postnatal_report_'.date(" F ", mktime(0, 0, 0, request('month'), 10)).'_'.request('year').'.pdf';
-
         $pdf = PDF::loadview('reports.postnatal');
         $pdf->save(storage_path('/reports/postnatal/'.$file_name));
         session()->flash('flash_message', 'Ripoti imeshaandaliwa!');
         return redirect('homepage');
-
-
     }
 
 //print the ivd activities report
     public function ivd_activities()
     {
-        echo 'ivd activities';
+        return view('reports.IVD_activities');
     }
 
 //print ufuatiliaji mtoto report
@@ -58,58 +52,9 @@ class ReportsController extends Controller
     public function antenatal()
     {
         $file_name = 'antenatal_report_'.date(" F ", mktime(0, 0, 0, request('month'), 10)).'_'.request('year').'.pdf';
-
         $pdf = PDF::loadview('reports.antenatal');
         $pdf->save(storage_path('/reports/antenatal/'.$file_name));
         session()->flash('flash_message', 'Ripoti imeshaandaliwa!');
         return redirect('homepage');
     }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    // ripoti ufuatliaji wa mtoto
-    public function mtotoPrint()
-    {
-
-        $pdf = PDF::loadview('reports.ufuatiliaji_mtoto');
-        return $pdf->download('ufuatiliaji_mtoto.pdf');
-    }
-
-    // ripoti ya mama na mtoto baada ya kujifungua
-    public function postnatalPrint()
-    {
-        $values = request();
-
-        $pdf = PDF::loadview('templates.postnatal', compact('values'));
-        return $pdf->download('postnatalShow.pdf');
-        
-    }
-
-    // report on IVD activities
-    public function IVDactivitiesPrint()
-    {
-        $pdf = PDF::loadview('reports.IVD_activities');
-        return $pdf->download('IVD_activities.pdf');
-    }
-
-    // taarifa ya mwezi toka kliniki (ANC)
-    public function antenatalPrint()
-    {
-
-        $pdf = PDF::loadview('reports.antenatal');
-        return $pdf->download('antenatal.pdf');
-    }
-}        // $maelezo = Maelezo_view::whereyear('tarehe_ya_hudhurio', $year)->wheremonth('tarehe_ya_hudhurio', $month)->get();
+}
