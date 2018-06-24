d8d9f6d81560776e06ca653079569a73e181ea3a
“[emma255]” <[aemmanuel199@gmail.com>
2018-06-24 17:48:36 +0300

    add status column

--- 2014_10_12_000000_create_users_table.php
+++ 2014_10_12_000000_create_users_table.php
@@ -25,6 +25,7 @@ class CreateUsersTable extends Migration
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
+            $table->string('status')->default('active');
         });
     }
 
