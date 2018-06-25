ab43f075673d0588aae88f2205c2f7cf2892fdc0
“[emma255]” <[aemmanuel199@gmail.com>
2018-06-25 05:17:17 +0300

    adding a division for the error

--- resources/views/error-view.blade.php
+++ resources/views/error-view.blade.php
@@ -1,8 +1,36 @@
-@include('layouts.head')
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Manehmos | Error</title>
+    <style>
+        html,
+        body {
+            height: 100%;
+            background-color: gainsboro;
+        }
+
+        .container {
+            height: 100%;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            font-family: sans-serif;
+            font-size: 40px
+        }
+    </style>
+</head>
+
 <body>
-    <div class="col-lg-offset-4">
-    {{$error_txt}}
+    <div class="container">
+        <div class="panel-warning">
+            {{$error_txt}}
+        </div>
 
     </div>
 </body>
+
 </html>
