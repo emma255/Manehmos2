0aea48ced714e0c37e5304aed554484647c9742a
“[emma255]” <[aemmanuel199@gmail.com>
2018-06-27 02:34:59 +0300

    adding loged in user in the main panel

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -43,7 +43,7 @@
                         <li class="nav-item dropdown">
                             <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true"
                                 aria-expanded="false" v-pre>
-                                    {{ Auth::user()->full_name }} <span class="caret"></span>
+                                    {{ Auth::user()->name }} <span class="caret"></span>
                                 </a>
 
                             <div class="dropdown-menu" aria-labelledby="navbarDropdown">
