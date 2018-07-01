9dcb32166956c28e0f7f7b9dfa29074e13186da7
“[emma255]” <[aemmanuel199@gmail.com>
2018-07-01 21:53:54 +0300

    usersSeeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,5 +12,20 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
+        for ($i=0; $i < 2; $i++) { 
+
+	    	DB::table('users')->insert([
+
+            'name' => str_random(10),
+            'worker_id' => str_random(10),
+            'position' => str_random(10),
+            'email' => str_random(12).'@gmail.com',
+            'center' => str_random(10),
+            'district' => str_random(10),
+            'phone_no' => '0766457248',
+            'password' => bcrypt(str_random(10)),
+	        ]);
+
+    	}
     }
 }
