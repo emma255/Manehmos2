8cbef5c0ba3a4b54229ded75f0af523f03d852ae
“[emma255]” <[aemmanuel199@gmail.com>
2018-07-04 19:24:30 +0300

    remove password reset and adjust the login button

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -51,14 +51,14 @@
                         </div>
 
                         <div class="form-group row mb-0">
-                            <div class="col-md-8 offset-md-4">
+                            <div class="col-md-8 offset-md-6">
                                 <button type="submit" class="btn btn-primary">
                                     {{ __('Login') }}
                                 </button>
 
-                                <a class="btn btn-link" href="{{ route('password.request') }}">
+                                {{-- <a class="btn btn-link" href="{{ route('password.request') }}">
                                     {{ __('Forgot Your Password?') }}
-                                </a>
+                                </a> --}}
                             </div>
                         </div>
                     </form>
