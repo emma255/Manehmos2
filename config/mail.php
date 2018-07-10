a350803cdd00d33073bd987ccc5a762c9bfe1605
“[emma255]” <[aemmanuel199@gmail.com>
2018-07-11 00:33:39 +0300

    sending emails

--- config/mail.php
+++ config/mail.php
@@ -56,7 +56,7 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'manehmos@hotmail.com'),
+        'address' => env('MAIL_FROM_ADDRESS', 'manehmos2@gmail.com'),
         'name' => env('MAIL_FROM_NAME', 'Manehmos'),
     ],
 
