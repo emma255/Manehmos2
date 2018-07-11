5884210d666aa701390a13139e234bec192dd781
“[emma255]” <[aemmanuel199@gmail.com>
2018-07-11 09:13:02 +0300

    change image of admin

--- resources/views/admin/main.blade.php
+++ resources/views/admin/main.blade.php
@@ -67,7 +67,7 @@
             <!-- User Account: style can be found in dropdown.less -->
             <li class="dropdown user user-menu">
               <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-                <img src="/imgs/emma.jpg" class="user-image" alt="User Image">
+                <img src="/imgs/emma.png" class="user-image" alt="User Image">
                 <span class="hidden-xs">{{Auth::user()->name}}</span>
               </a>
               <ul class="dropdown-menu">
@@ -96,7 +96,7 @@
         <!-- Sidebar user panel -->
         <div class="user-panel">
           <div class="pull-left image">
-            <img src="/imgs/emma.jpg" class="img-circle" alt="User Image">
+            <img src="/imgs/emma.png" class="img-circle" alt="User Image">
           </div>
           <div class="pull-left info">
             <p>{{Auth::user()->name}}</p>
