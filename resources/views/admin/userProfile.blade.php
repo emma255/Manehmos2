d8ca4b1c0202a6ec53ce14d3d7bdddb2ed5badba
“[emma255]” <[aemmanuel199@gmail.com>
2018-07-11 18:34:41 +0300

    add email in profile

--- resources/views/admin/userProfile.blade.php
+++ resources/views/admin/userProfile.blade.php
@@ -15,7 +15,7 @@
           <label for="name" style="font-size: 150%; font-style: italic">Is a {{$profile->position}} at {{$profile->district}} District</label><br>
           <label for="name" style="font-size: 150%; font-style: italic"><i class="fa fa-id-card"></i> {{$profile->worker_id}}</label><br>
           <label for="name" style="font-size: 150%; font-style: italic"><i class="fa fa-phone"></i> {{$profile->phone_no}}</label><br>
-          <label for="name" style="font-size: 150%; font-style: italic"><i class="fa fa-envelope"></i> {{$profile->phone_no}}</label>
+          <label for="name" style="font-size: 150%; font-style: italic"><i class="fa fa-envelope"></i> {{$profile->email}}</label>
         </div>
         <!-- /.box-body -->
         <div class="box-footer"></div>
