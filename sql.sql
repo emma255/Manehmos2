a1e67768841d066a505f9f3a10fe68f4308164a8
“[emma255]” <[aemmanuel199@gmail.com>
2018-07-12 06:57:26 +0300

    wanted query

--- sql.sql
+++ sql.sql
@@ -106,3 +106,5 @@ CREATE VIEW antenatal_malaria AS
 SELECT register6s.tarehe_ya_hudhurio, register_maternals.namba_ya_usajili, DATEDIFF(register_maternals.lnmp,register_maternals.tarehe_ya_kuzaliwa) as umri,
 register6s.llin, register6s.mrdt_o_bs, register6s.ipt, register6s.albendazole_mebendazole, register6s.vidonge_vya_I_FA, register6s.rufaa_alikopelekwa, register6s.sababu_ya_rufaa FROM register_maternals
 INNER JOIN register6s WHERE register_maternals.namba_ya_usajili = register6s.namba_ya_usajili;
+
+App\Infant::where('aina','awali')->groupby('namba_ya_usajili')->havingRaw('COUNT(DISTINCT hudhurio) =4')->pluck('Namba_ya_usajili')
