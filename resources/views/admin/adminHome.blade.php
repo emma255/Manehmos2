6b5af4de16061faae86be22853f6d4a1f6f329a3
“[emma255]” <[aemmanuel199@gmail.com>
2018-07-17 19:21:11 +0300

    adding the routes in the calendar

--- resources/views/admin/adminHome.blade.php
+++ resources/views/admin/adminHome.blade.php
@@ -18,10 +18,10 @@
           <button type="button" class="btn btn-success btn-sm dropdown-toggle" data-toggle="dropdown">
                     <i class="fa fa-bars"></i></button>
           <ul class="dropdown-menu pull-right" role="menu">
-            <li><a href="#">Add new event</a></li>
-            <li><a href="#">Clear events</a></li>
+            <li><a href="{{ asset('/task/create')}}">Add new event</a></li>
+            <li><a href="{{ asset('/showTasks')}}">Clear events</a></li>
             <li class="divider"></li>
-            <li><a href="#">View calendar</a></li>
+            {{-- <li><a href="#">View calendar</a></li> --}}
           </ul>
         </div>
         <button type="button" class="btn btn-success btn-sm" data-widget="collapse"><i class="fa fa-minus"></i>
