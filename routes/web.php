2569c679ccf21a57b07534200d9cec76c84eb2b5
“[emma255]” <[aemmanuel199@gmail.com>
2018-07-17 10:47:21 +0300

    registering middlewares

--- routes/web.php
+++ routes/web.php
@@ -11,7 +11,7 @@
 |
  */
 
-Route::get('homepage', 'HomeController@home')->middleware('auth');
+Route::get('homepage', 'HomeController@home')->middleware('clinician');
 
 Auth::routes();
 
@@ -54,27 +54,27 @@ Route::get('registrationForm', 'RegistrationFormController@create')->middleware(
 Route::get('user/profile', 'AdminHomeController@userProfile')->middleware('auth');
 
 // start reports routes
-Route::get('pdf', 'PDFController@index');
+Route::get('pdf', 'PDFController@index')->middleware('auth');
 
-Route::get('pdf/export', 'PDFController@export');
+Route::get('pdf/export', 'PDFController@export')->middleware('auth');
 
-Route::post('report/request', 'ReportsController@testcase');
+Route::post('report/request', 'ReportsController@testcase')->middleware('auth');
 
-Route::get('reportHome', 'ReportsController@index');
+Route::get('reportHome', 'ReportsController@index')->middleware('auth');
 
-Route::get('mtoto/print', 'ReportsController@mtotoPrint');
+Route::get('mtoto/print', 'ReportsController@mtotoPrint')->middleware('auth');
 
-Route::get('IVDactivities/print', 'ReportsController@IVDactivitiesPrint');
+Route::get('IVDactivities/print', 'ReportsController@IVDactivitiesPrint')->middleware('auth');
 
-Route::get('antenatal/print', 'ReportsController@antenatalPrint');
+Route::get('antenatal/print', 'ReportsController@antenatalPrint')->middleware('auth');
 
-Route::get('reports/postnatal', 'ReportsController@postnatal')->name('postnatalReport');
+Route::get('reports/postnatal', 'ReportsController@postnatal')->name('postnatalReport')->middleware('auth');
 
-Route::get('reports/IVD_activities', 'ReportsController@ivd_activities')->name('ivd');
+Route::get('reports/IVD_activities', 'ReportsController@ivd_activities')->name('ivd')->middleware('auth');
 
-Route::get('reports/ufuatiliaji_mtoto', 'ReportsController@ufuatiliaji_mtoto')->name('ufuatiliaji_mtoto');
+Route::get('reports/ufuatiliaji_mtoto', 'ReportsController@ufuatiliaji_mtoto')->name('ufuatiliaji_mtoto')->middleware('auth');
 
-Route::get('reports/antenatal', 'ReportsController@antenatal')->name('antenatal');
+Route::get('reports/antenatal', 'ReportsController@antenatal')->name('antenatal')->middleware('auth');
 
 // end reports routes
 
@@ -99,4 +99,4 @@ Route::post('child/store', 'RegisterChildController@store')->middleware('auth');
 
 Route::post('task/store', 'TasksController@store')->middleware('auth');
 
-Route::post('postnatal/print', 'ReportsController@postnatalPrint');
+Route::post('postnatal/print', 'ReportsController@postnatalPrint')->middleware('auth');
